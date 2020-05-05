94ac73b00325030b95107178c7c1c8ebdc149b1b
tomi09pl <tomi09pl@gmail.com>
2020-05-06 00:02:19 +0100

    form validation in progress

--- src/components/HomeContact.js
+++ src/components/HomeContact.js
@@ -8,7 +8,12 @@ export default class HomeContact extends React.Component {
         this.state = {
             name: '',
             email: '',
-            message: ''
+            message: '',
+            errors: {
+                name:false,
+                email:false,
+                message:false
+            }
         }
 
     }
@@ -17,6 +22,43 @@ export default class HomeContact extends React.Component {
         this.setState({
             [e.target.name]: e.target.value
         })
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log('polecialo!');
+        const {name, value} = e.target;
+        let errors = this.state.errors;
+
+        //RegEx cases//
+        let nameRegex=/^[a-zA-Z]{1,40}$/;
+        let emailRegex= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let messageRegex=/^.{120,1000}$/;
+        //=========//
+
+        switch (name) {
+            case 'name':
+                console.log(nameRegex.test(value));
+                errors.name = nameRegex.test(value);
+                break;
+
+            case 'email':
+                console.log(emailRegex.test(value));
+                errors.name = emailRegex.test(value);
+                break;
+
+            case 'message':
+                console.log(messageRegex.test(value));
+                errors.name = messageRegex.test(value);
+                break;
+
+            default:
+                break;
+        }
+
+        this.setState ({
+            errors, [name]: value,}, ()=>{console.log(errors)})
+
     };
 
     render() {
@@ -36,14 +78,14 @@ export default class HomeContact extends React.Component {
                                 </div>
                                 <div className="txtField">
                                     <label htmlFor="contactEmail">Enter your e-mail</label>
-                                    <input type="email" placeholder='yourEmail@mail.com' name='email'
+                                    <input type="text" placeholder='yourEmail@mail.com' name='email'
                                            value={this.state.email} onChange={this.handleChange}/>
                                 </div>
                             </div>
                             <div className="contact-content">
                                 <label htmlFor="contactMessage">Enter your message</label>
                                 <textarea placeholder='Please start typing your message here...' name='message'
-                                          onChange={this.handleChange}/>
+                                          value={this.state.message} onChange={this.handleChange}/>
                             </div>
                             <div className="contact-submit">
                                 <input type="submit" value='Send'/>
