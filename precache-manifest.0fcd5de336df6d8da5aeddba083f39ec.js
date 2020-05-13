327b90ea492c0e2b59d13edc28ff35c1f9164e23
tomi09pl <tomi09pl@gmail.com>
2020-05-13 01:02:31 +0100

    Updates

---
+++ precache-manifest.0fcd5de336df6d8da5aeddba083f39ec.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f1b5b33879c44e4d07d9ce72f30897",
+    "url": "/give_away/index.html"
+  },
+  {
+    "revision": "7f83a0c50a0d5ff6a73f",
+    "url": "/give_away/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ad378a3234a33087f819",
+    "url": "/give_away/static/css/main.83d1aba7.chunk.css"
+  },
+  {
+    "revision": "7f83a0c50a0d5ff6a73f",
+    "url": "/give_away/static/js/2.d21f059a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/give_away/static/js/2.d21f059a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad378a3234a33087f819",
+    "url": "/give_away/static/js/main.581eca39.chunk.js"
+  },
+  {
+    "revision": "21d0273c9fe71d363444",
+    "url": "/give_away/static/js/runtime-main.36bd74f9.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/give_away/static/media/3ColumnsBackground.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/give_away/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/give_away/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/give_away/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/give_away/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/give_away/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/give_away/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/give_away/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/give_away/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/give_away/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/give_away/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/give_away/static/media/Signature.e54cfcfc.svg"
+  }
+]);
